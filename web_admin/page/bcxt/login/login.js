05bca67b535714fb7c53beee31394a9cd5ec8939
lingfe <13068326391@163.com>
2021-02-23 14:18:12 +0800

    update Signed-off-by: lingfe <13068326391@163.com>

--- web_admin/page/bcxt/login/login.js
+++ web_admin/page/bcxt/login/login.js
@@ -1,6 +1,6 @@
 const Storage =  {}
 Storage.get = function (name) {
-    return JSON.parse(localStorage.getItem(name))
+    return localStorage.getItem(name);
 }
 Storage.set = function (name, val) {
     localStorage.setItem(name, JSON.stringify(val))
