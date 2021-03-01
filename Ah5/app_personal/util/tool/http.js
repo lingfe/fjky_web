7ca706e85baa6fd249c7d2c072251894a4000f9c
qiuyuling152 <15286753606@163.com>
2021-03-01 17:47:01 +0800

    上传打包后的文件

---
+++ Ah5/app_personal/util/tool/http.js
@@ -0,0 +1,75 @@
+let http = {};
+export default http = {
+	//get请求方法
+	Get: function(url, data, getData) {
+		uni.showLoading({
+			title: '加载中',
+			iocn: 'none',
+		});
+		uni.request({
+			url: url,
+			method: 'GET',
+			data: data,
+			header: {
+				//自定义请求头信息
+				'token': ''
+			},
+			timeout:1000, 
+			success: (res) => {
+				uni.hideLoading();
+				if (res.data.code == 0){
+					getData(res.data);
+				} else {
+					uni.showToast({
+						title: res.msg ||url+ '请求失败',
+						duration: 2000,
+						icon:"none",
+					});
+				}
+
+			},
+			fail: (err) => {
+				console.log(err);
+				uni.showToast({
+					title: err.msg,
+					duration: 1000
+				});
+				uni.hideLoading(); 
+			},
+			complete: () => {
+				uni.hideLoading();
+			}
+		});
+	},
+	//post请求方法
+	Post: function(url, data,getData) {
+		uni.showLoading({
+			title: '加载中',
+			iocn: 'none',
+		});
+		uni.request({
+			url: url,
+			method: 'POST',
+			data: data,
+			timeout:1000,
+			header: {
+				//自定义请求头信息
+				'token': ''
+			},
+			success: (res) => {
+				uni.hideLoading();
+				if (res.data.code == 0) {
+					getData(res.data);
+				} else {
+					uni.showToast({
+						title: res.msg,
+						duration: 1000
+					});
+				}
+			},
+			complete: () => {
+				uni.hideLoading();
+			}
+		});
+	}
+}
